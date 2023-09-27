5420abd38ef97c18c7866359309c0a88d60d0c9c
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-09-27 17:02:26 +0300

    Exercise Basic Syntax, Conditional Statements, and Loops / multiplicationTable

---
+++ Exercise Basic Syntax, Conditional Statements, and Loops/multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplication (input){
+    for (let index = 1; index <= 10; index++) {
+        let result = index * input;
+        console.log(`${input} X ${index} = ${result}`);
+        
+    }
+}
+multiplication(5)
