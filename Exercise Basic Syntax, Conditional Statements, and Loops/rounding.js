f9e82d900dfb477addd84ef1e5d15600ea055a5b
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2023-10-09 22:34:58 +0300

    Exercise Basic Syntax, Conditional Statements, and Loops / rounding

---
+++ Exercise Basic Syntax, Conditional Statements, and Loops/rounding.js
@@ -0,0 +1,10 @@
+function solve (num , precision){
+    let a = Number(num);
+    let b = Number(precision);
+    if (b > 15){
+       b = 15;
+    }
+    a = a.toFixed(b);
+    console.log(parseFloat(a));
+   }
+   solve()
